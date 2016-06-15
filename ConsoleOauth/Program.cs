508c1bcce11fa752671dba191f7e6ef15b4e1011
Huang Yuan haung <dha834773759@163.com>
2016-06-15 23:46:09 +0800

    Oauth

---
+++ ConsoleOauth/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+
+namespace ConsoleOauth
+{
+    class Program
+    {
+        private static HttpClient httpClient=new HttpClient();
+        static void Main(string[] args)
+        {
+            httpClient.BaseAddress = new Uri("http://localhost/authority");
+            GetAccessTokenByCredentials();
+        }
+
+        static void GetAccessTokenByCredentials()
+        {
+            var parms = new Dictionary<string, string>();
+            parms.Add("client_id", "1234");
+            parms.Add("client_secret", "5678");
+            parms.Add("grant_type","client_credentials");
+            Console.WriteLine(httpClient.PostAsync("/authority/token", new FormUrlEncodedContent(parms)).Result.Content.ReadAsStringAsync().Result);
+            Console.ReadKey();
+        }
+    }
+}
